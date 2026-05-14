28207f0ca76101fe4433f015e1611a5d81b0107a
Azathoth <imperialbenjie393@gmail.com>
2026-05-14 20:23:02 +0800

    chore: added service account credentials

--- src/core/settings.py
+++ src/core/settings.py
@@ -9,6 +9,21 @@ class Settings(BaseSettings):
     
     UPSTASH_REDIS_REST_URL: str | None = None
     UPSTASH_REDIS_REST_TOKEN: str | None = None
+    
+    TYPE: str | None = None 
+    PROJECT_ID: str | None = None 
+    PRIVATE_KEY_ID: str | None = None 
+    PRIVATE_KEY: str | None = None 
+    CLIENT_EMAIL: str | None = None 
+    CLIENT_ID: str | None = None 
+    AUTH_URI: str | None = None 
+    TOKEN_URI: str | None = None 
+    AUTH_PROVIDER_X509_CERT_URL: str | None = None 
+    CLIENT_X509_CERT_URL: str | None = None 
+    UNIVERSE_DOMAIN: str | None = None
+    
+    GCS_BUCKET_NAME: str | None = None
+    GCS_SIGNED_URL_EXPIRATION: int | None = None
 
     model_config = SettingsConfigDict(
         env_file=".env",
